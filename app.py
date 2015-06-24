c306e1bafb7fa0e3e84078167d9b3f1af5612c61
Adinan <adinan@IDC7461.local>
2015-06-24 12:42:06 -0300

    Return json

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from unidecode import unidecode
 from utils import GoogleSearch, GetBestAnswer
 import os
 
@@ -10,8 +11,10 @@ def search(query):
     question = GoogleSearch(query)
     q = question['responseData']['results'][0]
     res = GetBestAnswer(q['unescapedUrl'])
-    return res
+#    return "%s\n%s" % (unidecode(q['title']), res)
+    return jsonify(bestanswer=res)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
--- utils.py
+++ utils.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python
 
+from urllib2 import urlopen
+from BeautifulSoup import BeautifulSoup
 import requests
-import urllib2
 import re
-
 
 def GoogleSearch(query):
     return(requests.get("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=site:br.answers.yahoo.com %s" % (query)).json())
 
 
 def GetBestAnswer(answerurl):
-#    htmlsource = urllib2.urlopen("https://br.answers.yahoo.com/question/index?qid=20130314111535AADyGFC").read()
-    htmlsource = urllib2.urlopen(answerurl).read()
-    t = False
+    htmlsource = urlopen(answerurl).read()
+    assoup = BeautifulSoup(htmlsource)
+    bestanswer = assoup.find('span', attrs={'class': 'ya-q-full-text'})
 
-    l = ''
-    for line in htmlsource.splitlines():
-        if 'Melhor resposta:' in line:
-            t = True
-            continue
-
-        if t:
-            l += line
-            if '</span>' in line:
-                break
-
-    TAG_RE = re.compile(r'<[^>]+>')
-    b = TAG_RE.sub('', l)
-    return(b)
+    return(bestanswer.text)
